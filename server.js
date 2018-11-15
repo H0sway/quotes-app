3135ed80b102fa0bbe891d3a50cd6c7681ebe044
Joshua Russell <jkrussell756@gmail.com>
2018-11-15 11:14:55 -0500

    Installed express, set up the server

---
+++ server.js
@@ -0,0 +1,13 @@
+// server.js
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
+
+app.listen(PORT, () => {
+  console.log(`App is up and running. Listening on port ${PORT}`);
+});
