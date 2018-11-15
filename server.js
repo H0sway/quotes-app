0d6f7c5fcf652067ff87817c6d56434f1944123f
Joshua Russell <jkrussell756@gmail.com>
2018-11-15 15:06:10 -0500

    added index.html file, configured the server for react

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 // server.js
+
 const express = require('express');
 const app = express();
 
 // Set the port
 const PORT = process.env.PORT || 3000;
 
+// Import Path
+const path = require('path');
+
+// Static files
+app.use(express.static('build'));
+
 // API Routes
 app.use('/api/quotes', require('./routes/quote-routes'));
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'))
 });
 
 app.listen(PORT, () => {
