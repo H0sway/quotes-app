2ddaeda4cba34363d0cafcbf54fab2829f1a47fb
Joshua Russell <jkrussell756@gmail.com>
2018-11-15 13:51:22 -0500

    Model methods added.

---
+++ models/quote.js
@@ -0,0 +1,31 @@
+// models/quotes.js
+
+// Import the database
+const db = require('../db/config');
+
+// Instantiate a new object.
+// This will interface with the quotes-app database.
+// Specifically, the quotes table.
+const Quote = {};
+
+// Define methods for the Quote object
+
+// Returns all quotes from the table
+Quote.findAll = () => {
+  return db.query(
+    `SELECT * FROM quotes`
+  );
+};
+
+// Return one quote with the specific id
+Quote.findById = (id) => {
+  return db.oneOrNone(
+    `
+      SELECT * FROM quotes
+      WHERE id = $1
+    `
+  );
+};
+
+// Export the Quote object
+module.exports = Quote;
