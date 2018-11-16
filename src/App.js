0fdf0aa98fb730815c61b225b424ddbdb9303917
Joshua Russell <jkrussell756@gmail.com>
2018-11-15 20:41:09 -0500

    added the home component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@
 // Import react and the component class
 import React, { Component } from 'react';
 // Import BrowserRouter
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+// import each component
+import Home from './components/Home';
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
-          
+
+          <Route exact path="/" component={Home} />
+
         </div>
       </Router>
     )
---
+++ src/components/Home.js
@@ -0,0 +1,65 @@
+// src/components/Home.js
+
+// Import react
+import React, { Component } from 'react';
+// Axios for making GET requests
+import axios from 'axios';
+
+class Home extends Component {
+  constructor() {
+    super();
+    // Define state
+    this.state = {
+      quotes: [],
+      dataLoaded: false
+    }
+  }
+
+  // Is called when the component succesfully loads
+  componentDidMount() {
+    // GET request to our server
+    axios({
+      method: 'GET',
+      url: '/api/quotes'
+    })
+    // Saves the data to state. Only way to change the state is with setState
+    .then(data => {
+      console.log(data.data.data);
+      this.setState({
+        quotes: data.data.data,
+        dataLoaded: true
+      });
+    })
+    // logs an error
+    .catch(err => {
+      console.log(err);
+    });
+  }
+
+  renderQuotes() {
+    if (this.state.dataLoaded) {
+      return this.state.quotes.map(quote => {
+        return (
+          <div key={quote.id}>
+            <p>{quote.quote}</p>
+            <p>{quote.author}</p>
+          </div>
+        )
+      })
+    }
+    else {
+      <p>Loading...</p>
+    }
+  }
+
+  render() {
+    return (
+      <div className="Home">
+        <h3>These are my favorite quotes</h3>
+        {this.renderQuotes()}
+      </div>
+    )
+  }
+};
+
+export default Home;
